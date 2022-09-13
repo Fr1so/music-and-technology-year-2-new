674469b469bd23b76ac1d5d4920f6e9d2a109144
Friso van Beek <frisfro1998@gmail.com>
2022-09-13 10:07:16 +0200

    Replaced sound with kick

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,5 @@
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("/home/fr1so/Documents/CSD2/music-and-technology-year-2-new/python_basics/Tuk_1.wav")
+wave_obj = sa.WaveObject.from_wave_file("/home/fr1so/Documents/CSD2/music-and-technology-year-2-new/python_basics/kick.wav")
 play_obj = wave_obj.play()
 play_obj.wait_done()
