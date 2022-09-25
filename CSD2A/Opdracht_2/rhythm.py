bff9c1739cda83e42504ce9c79e0436022c4eb10
Friso van Beek <frisfro1998@gmail.com>
2022-09-25 21:48:49 +0200

    Added all inputs

--- CSD2A/Opdracht_2/rhythm.py
+++ CSD2A/Opdracht_2/rhythm.py
@@ -2,35 +2,55 @@
 ## Friso's Rhythmic Playback ##
 ###############################
 
-# Importing libraries
+#######################
+# Importing libraries #
+#######################
 
 import simpleaudio as sa
 import time
 
-# Sample location
+###################
+# Sample location #
+###################
 
 wave_obj = sa.WaveObject.from_wave_file("../assets/kick.wav")
 
-# Define sample playing function
+##################################
+# Define sample playing function #
+##################################
 
 def playSample(amount):
     for x in range(amount):
         play_obj = wave_obj.play()
         play_obj.wait_done()
 
-# User interaction
+####################
+# User interaction #
+####################
+
+# Ask user for amount of times, length (with 1 being quarter note) and bpm #
 
 numPlaybackTimes = int(input("Please enter the amount of times you would like for the sample to be played: "))
 
 print(numPlaybackTimes, "times.")
 
-noteDurations = []
+noteDurationsList = []
 
 for i in range(numPlaybackTimes):
-    noteDurations.append(float(input("Enter the duration of the notes: ")))
+    noteDuration = (float(input("Please enter the duration of the notes as a float: ")))   
+    noteDurationsList.append(noteDuration)
+        
+print(noteDurationsList)
 
-print(noteDurations)
+bpm = float(input("Please enter the bpm: "))
+quarterNote = 60.0 / bpm
 
-# Run sample playing function
+print("Bpm: ", bpm, "Quarternote duration: ", quarterNote, "sec")
+
+
+
+################################
+# Run rhythmic playback engine #
+################################
 
 playSample(numPlaybackTimes)
